b0667b6677beeccf1db8f10e6ed12925f04af43e
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-29 18:24:59 +0000

    Automatically backed up by Learn

--- db/migrate/20200729181822_create_dogs.rb
+++ db/migrate/20200729181822_create_dogs.rb
@@ -1,14 +1,10 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
   
-  def up 
+  def change
     create_table :dogs do |column|
       column.string :name 
       column.string :breed
     end
-  end
-  
-  def down 
-    drop_table :dogs 
   end
   
 end
